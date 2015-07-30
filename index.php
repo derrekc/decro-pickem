87f8636664f169dbae0cdbf4a3b2bb9eee0702d1
Derrek Croney <derrek@decro.net>
2015-07-30 11:20:02 -0400

    nothing, really

--- index.php
+++ index.php
@@ -3,15 +3,14 @@
 // Retrieve instance of the framework
 $f3=require('lib/base.php');
 
-\Template::instance()->extend('pagebrowser', '\Pagination::renderTag');
-
 // Initialize CMS
 $f3->config('app/config.ini');
 
 // Define routes
 $f3->config('app/routes.ini');
 
-$f3->route('GET @welcome: /', 'DashboardController->home');
+\Template::instance()->extend('pagebrowser', '\Pagination::renderTag');
 
 // Execute application
 $f3->run();
+
