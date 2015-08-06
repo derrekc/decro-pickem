1ee28461dc02e1c04baaa61785e629ade7d70011
Derrek Croney <derrek@decro.net>
2015-08-06 11:56:27 -0400

    added player status hook

--- app/admin/pickemcontroller.php
+++ app/admin/pickemcontroller.php
@@ -1,7 +1,7 @@
 <?php
 namespace Admin;
 
-class PickemController extends \DashboardController {
+class PickemController extends \AdminController {
 
 	function home($f3) {
 		$f3->set('page_title', "Pickem Management, etc");
@@ -25,4 +25,30 @@ class PickemController extends \DashboardController {
 		$f3->set('SESSION.success_message', sprintf("Picks have been cleared for week %s", $target_week));
 		$f3->set('inc', 'admin/pickem_stuff.htm');
 	}
+
+	function playerStatus($f3) {
+		$q = "SELECT *, DATE_FORMAT(FROM_UNIXTIME(completed_stamp), '%b %e - %r') AS completed_datetime FROM pickem_player_data WHERE pickem_id = ?";
+		$players = $this->db->exec($q, $f3->get('pickem.default_pickem_id'));
+		#echo print_r($players, true);
+
+		$f3->set('players', $players);
+		$f3->set('inc', 'admin/player_status.htm');
+	}
+
+	function importAPPoll($f3) {
+		header("Content-type: application/json", TRUE);
+		$this->use_json = TRUE;
+
+		if ($f3->exists('FILES.upload_file', $upload_file)) {
+			if (is_uploaded_file($upload_file['tmp_name'])) {
+				if (($handle = fopen($upload_file['tmp_name'])) !== FALSE) {
+					while (($data = fgetcsv($handle, 1024, ','))) {
+						echo json_encode($data) . "\n";
+					}
+					fclose($handle);
+				}
+			}
+		}
+	}
+
 }
